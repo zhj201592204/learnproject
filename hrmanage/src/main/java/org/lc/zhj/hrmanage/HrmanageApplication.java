8d661c6e4533ac088b89126bd93f304973b2e539
zhouhongjin <1099315974?zhj>
2020-12-26 15:48:56 +0800

    create：微人事项目初始化

---
+++ hrmanage/src/main/java/org/lc/zhj/hrmanage/HrmanageApplication.java
@@ -0,0 +1,13 @@
+package org.lc.zhj.hrmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmanageApplication.class, args);
+    }
+
+}
---
+++ hrmanage/src/test/java/org/lc/zhj/hrmanage/HrmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lc.zhj.hrmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
